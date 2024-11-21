59339882ffa680575a441cd426ac5800a4e6a211
TrentonOttman <trentondavidottman@gmail.com>
2024-11-21 12:28:04 -0600

    comments and constants

--- src/main.py
+++ src/main.py
@@ -68,13 +68,19 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         generate_pages_recursive(path, template_path, destination)
 
 def main():
+    # Source directory to copy from. Include CSS, images, etc here
     SOURCE_PATH = "static"
+    # Destination directory to copy to.
     DESTINATION_PATH = "public"
+    # Content directory containing markdown files to be converted.
+    CONTENT_PATH = "content"
+    # Template HTML file to use for formatting
+    TEMPLATE_PATH = "template.html"
     
     shutil.rmtree(DESTINATION_PATH)
     os.mkdir(DESTINATION_PATH)
     source_to_destination(SOURCE_PATH, DESTINATION_PATH)
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, DESTINATION_PATH)
 
 if __name__ == "__main__":
     main()
